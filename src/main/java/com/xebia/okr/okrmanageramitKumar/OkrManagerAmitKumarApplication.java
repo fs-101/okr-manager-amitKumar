ceb79458a5cb26d14a82d4f33fc6619b18dc083d
amit88265 <amitkumar88265@gmail.com>
2019-08-26 11:17:46 +0530

    add spring starter web files

---
+++ src/main/java/com/xebia/okr/okrmanageramitKumar/OkrManagerAmitKumarApplication.java
@@ -0,0 +1,13 @@
+package com.xebia.okr.okrmanageramitKumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrManagerAmitKumarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrManagerAmitKumarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xebia/okr/okrmanageramitKumar/OkrManagerAmitKumarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xebia.okr.okrmanageramitKumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkrManagerAmitKumarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
